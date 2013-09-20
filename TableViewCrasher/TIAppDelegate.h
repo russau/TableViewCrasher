81aae33e403945909f1d41af17b82f3134a81230
russ <russell.sayers@gmail.com>
2013-09-20 09:28:10 -0700

    Initial Commit

---
+++ TableViewCrasher/TIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TIAppDelegate.h
+//  TableViewCrasher
+//
+//  Created by Sayers, Russell on 9/20/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TableViewCrasher/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TableViewCrasher
+//
+//  Created by Sayers, Russell on 9/20/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TIAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TIAppDelegate class]));
+    }
+}
